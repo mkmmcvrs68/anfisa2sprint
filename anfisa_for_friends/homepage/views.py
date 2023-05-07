94b1873cc81b8205b8c9ff4671c059e18833257a
Максим Козлов <mkvrs68@gmail.com>
2023-05-07 09:31:54 +0300

    anfisa

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+from django.db.models import Q
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+        ).filter(
+            is_published=True,
+            is_on_main=True,
+            category__is_published=True  # Категория разрешена к публикации
+            )
+    return render(request, template, {'ice_cream_list': ice_cream_list})
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
-
+from .models import Category, Wrapper, Topping, IceCream
 # Register your models here.
+
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Wrapper)
+admin.site.register(Topping)
